8184247659f18f92125b44764ea42f301ef78c6b
Eslam Mohamed <eslammohamedmetwally1@gmail.com>
2023-09-27 21:43:20 +0300

    first step

---
+++ portfolio/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+
+class Portfolio(models.Model):
+    pass
+
+
+class Skills(models.Model):
+    pass
+
+
+class Tools(models.Model):
+    pass
+
+class Projects(models.Model):
+    pass
+
+class Colleagues(models.Model):
+    pass
